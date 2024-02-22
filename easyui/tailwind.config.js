ecee4e855b61ab0035e211cbdc92cfb7de671e28
AZ <47402666+realazaber@users.noreply.github.com>
2024-02-22 16:07:43 +0100

    demo files

---
+++ easyui/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#895afe",
+        secondary: "#5edce7",
+        accent: "#0F172A",
+      },
+    },
+  },
+  plugins: [],
+};

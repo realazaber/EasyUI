e87f82ff994cd03b32a8c10b80c6e5d6f1b6661f
AZ <47402666+realazaber@users.noreply.github.com>
2024-03-01 01:32:20 +0100

    Card component

---
+++ easyui/projects/ngeasy-ui/src/lib/components/card/card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { cLayoutBase } from '../../classes/cLayoutBase';
+
+@Component({
+  selector: 'eui-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './card.component.html',
+  styles: ``,
+})
+export class EuiCardComponent extends cLayoutBase {}

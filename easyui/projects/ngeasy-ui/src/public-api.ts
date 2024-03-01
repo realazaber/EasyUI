a3c0c0b82abc67f3a90b2c8970156853c14067b7
AZ <47402666+realazaber@users.noreply.github.com>
2024-03-01 10:10:47 +0100

    Update public-api.ts

--- easyui/projects/ngeasy-ui/src/public-api.ts
+++ easyui/projects/ngeasy-ui/src/public-api.ts
@@ -10,6 +10,11 @@ export * from './lib/components/section/section.component';
 export * from './lib/components/container/container.component';
 export * from './lib/components/badge/badge.component';
 export * from './lib/components/button/button.component';
+export * from './lib/components/card/card.component';
+export * from './lib/components/imgcard/imgcard.component';
+export * from './lib/components/row/row.component';
+export * from './lib/components/col/col.component';
+export * from './lib/components/grid/grid.component';
 
 export { eBgColorPick } from './lib/types/colorPick';
 export { eSizePick } from './lib/types/sizePick';

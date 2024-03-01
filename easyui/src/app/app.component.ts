09b19963606a4666c9d802e1844549745cef8d28
AZ <47402666+realazaber@users.noreply.github.com>
2024-03-01 10:10:41 +0100

    Update app.component.ts

--- easyui/src/app/app.component.ts
+++ easyui/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { EasyUIModule } from '@azaber/ngeasy-ui';
+import { EasyUIModule } from '../../projects/ngeasy-ui/src/public-api';
+import { NavComponent } from './components/nav/nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, EasyUIModule],
+  imports: [CommonModule, RouterOutlet, EasyUIModule, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })

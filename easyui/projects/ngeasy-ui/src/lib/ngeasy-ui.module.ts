2c8c23479a9307be5838440f670dc0a215704d52
AZ <47402666+realazaber@users.noreply.github.com>
2024-03-01 10:10:52 +0100

    Update ngeasy-ui.module.ts

--- easyui/projects/ngeasy-ui/src/lib/ngeasy-ui.module.ts
+++ easyui/projects/ngeasy-ui/src/lib/ngeasy-ui.module.ts
@@ -5,24 +5,38 @@ import { EuiBadgeComponent } from './components/badge/badge.component';
 import { EuiContainerComponent } from './components/container/container.component';
 import { EuiSectionComponent } from './components/section/section.component';
 import { EuiLayoutModule } from './modules/layout/layout.module';
-
+import { EuiCardComponent } from './components/card/card.component';
+import { EuiImgcardComponent } from './components/imgcard/imgcard.component';
+import { EuiRowComponent } from './components/row/row.component';
+import { EuiColComponent } from './components/col/col.component';
+import { EuiGridComponent } from './components/grid/grid.component';
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     EuiButtonComponent,
     EuiBadgeComponent,
+    EuiCardComponent,
+    EuiColComponent,
+    EuiRowComponent,
+    EuiImgcardComponent,
     EuiLayoutModule,
     EuiContainerComponent,
     EuiSectionComponent,
+    EuiGridComponent,
   ],
   exports: [
     CommonModule,
     EuiButtonComponent,
     EuiBadgeComponent,
+    EuiCardComponent,
+    EuiColComponent,
+    EuiRowComponent,
+    EuiImgcardComponent,
     EuiLayoutModule,
     EuiContainerComponent,
     EuiSectionComponent,
+    EuiGridComponent,
   ],
 })
 export class EasyUIModule {}

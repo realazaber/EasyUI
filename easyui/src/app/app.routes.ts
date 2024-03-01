e4ed0e052dc34e6399904b27e024ecd7b0adf193
AZ <47402666+realazaber@users.noreply.github.com>
2024-03-01 10:10:36 +0100

    Update app.routes.ts

--- easyui/src/app/app.routes.ts
+++ easyui/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { TestComponent } from './pages/test/test.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    title: 'home',
+    component: HomeComponent,
+    pathMatch: 'full',
+    path: '',
+  },
+
+  {
+    title: 'test',
+    component: TestComponent,
+    pathMatch: 'full',
+    path: 'test',
+  },
+];

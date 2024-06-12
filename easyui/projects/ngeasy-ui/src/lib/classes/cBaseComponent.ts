3eab89558470dd3c0ee9b178ba67a905e8db57a3
AZ <47402666+realazaber@users.noreply.github.com>
2024-06-13 01:01:15 +0200

    QOL: Added opacity and shortened size options

--- easyui/projects/ngeasy-ui/src/lib/classes/cBaseComponent.ts
+++ easyui/projects/ngeasy-ui/src/lib/classes/cBaseComponent.ts
@@ -8,6 +8,28 @@ import { eSizePick } from '../types/sizePick';
 export class cBaseComponent {
   @Input() bgColor?: eBgColorPick | string | null = null;
   @Input() size?: eSizePick | null = null;
+  @Input() opacity?:
+    | 0
+    | 5
+    | 10
+    | 15
+    | 20
+    | 25
+    | 30
+    | 35
+    | 40
+    | 45
+    | 50
+    | 55
+    | 60
+    | 65
+    | 70
+    | 75
+    | 80
+    | 85
+    | 90
+    | 95
+    | 100 = 0;
 
   getClasses(): string {
     let extraClasses: string = ' ';
@@ -41,20 +63,28 @@ export class cBaseComponent {
     }
     switch (this.size) {
       case 'Small':
+      case 'S':
         extraClasses += 'text-sm p-1';
         break;
       case 'Medium':
+      case 'M':
         extraClasses += 'text-md px-3 py-2';
         break;
       case 'Large':
+      case 'L':
         extraClasses += 'text-lg px-4 py-3';
         break;
       case 'XLarge':
+      case 'XL':
         extraClasses += 'text-xl px-5 py-4';
         break;
       default:
         extraClasses += 'p-1';
         break;
+    }
+
+    if (this.opacity) {
+      extraClasses += ' opacity-' + this.opacity + ' ';
     }
 
     return extraClasses;

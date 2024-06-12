ae35e82132e1ae1db36011a8063fb25d253bdd6b
AZ <47402666+realazaber@users.noreply.github.com>
2024-06-13 00:59:56 +0200

    Page Showcase: Badges

---
+++ easyui/src/app/pages/components/badges/badges.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { EasyUIModule } from 'ngeasy-ui';
+
+@Component({
+  selector: 'app-badges',
+  standalone: true,
+  imports: [EasyUIModule],
+  templateUrl: './badges.component.html',
+  styles: ``,
+})
+export class BadgesPage {}

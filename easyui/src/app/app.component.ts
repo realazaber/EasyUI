bac1c3d345ecf3aaa396691c365aa0899509c3e0
AZ <47402666+realazaber@users.noreply.github.com>
2024-06-13 01:02:36 +0200

    Redone app component

--- easyui/src/app/app.component.ts
+++ easyui/src/app/app.component.ts
@@ -2,13 +2,19 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
-//import { EasyUIModule } from '../../projects/ngeasy-ui/src/public-api';
-import { EasyUIModule } from '../../dist/ngeasy-ui';
+import { EasyUIModule } from 'ngeasy-ui';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, EasyUIModule, NavComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    NavComponent,
+    FooterComponent,
+    EasyUIModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })

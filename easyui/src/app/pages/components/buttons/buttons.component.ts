360b443e1513974768fbd591d3231034e4d1b242
AZ <47402666+realazaber@users.noreply.github.com>
2024-06-13 01:00:03 +0200

    Page Showcase: Buttons

---
+++ easyui/src/app/pages/components/buttons/buttons.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { EasyUIModule } from 'ngeasy-ui';
+
+@Component({
+  selector: 'app-buttons',
+  standalone: true,
+  imports: [EasyUIModule],
+  templateUrl: './buttons.component.html',
+  styles: ``,
+})
+export class ButtonsPage {}

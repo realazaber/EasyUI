49f4f9aca89de4412a9155dfaa9fbfde644e73ec
AZ <47402666+realazaber@users.noreply.github.com>
2024-06-13 01:02:08 +0200

    Feature: Added shortened selector options for size pick

--- easyui/projects/ngeasy-ui/src/lib/types/sizePick.ts
+++ easyui/projects/ngeasy-ui/src/lib/types/sizePick.ts
@@ -1,8 +1,12 @@
 const eSizePick = {
   Small: 'Small',
+  S: 'S',
   Medium: 'Medium',
+  M: 'M',
   Large: 'Large',
+  L: 'L',
   XLarge: 'XLarge',
+  XL: 'XL',
 } as const;
 
 export type eSizePick = (typeof eSizePick)[keyof typeof eSizePick];

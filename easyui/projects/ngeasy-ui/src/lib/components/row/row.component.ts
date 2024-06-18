236ae88c0e3ca3e14e222c470fde7fba5aceff59
AZ <47402666+realazaber@users.noreply.github.com>
2024-06-19 01:11:31 +0200

    Fix: Remove unneeded import in row component

--- easyui/projects/ngeasy-ui/src/lib/components/row/row.component.ts
+++ easyui/projects/ngeasy-ui/src/lib/components/row/row.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { cAlignBase } from '../../classes/cAlignBase';
 
 @Component({

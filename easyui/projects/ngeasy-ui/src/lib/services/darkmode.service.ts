ae828bb9209544ee59c4a5d885a1496a256c06b3
AZ <47402666+realazaber@users.noreply.github.com>
2024-06-21 20:22:20 +0200

    Dark mode: Now saves to local storage

--- easyui/projects/ngeasy-ui/src/lib/services/darkmode.service.ts
+++ easyui/projects/ngeasy-ui/src/lib/services/darkmode.service.ts
@@ -5,6 +5,7 @@ import { Injectable, Signal, signal, WritableSignal } from '@angular/core';
 })
 export class DarkmodeService {
   darkOn: WritableSignal<boolean> = signal(false);
+  key: string = 'eui-darkmode';
   toggleDiv: any = null;
   constructor() {}
 
@@ -13,16 +14,34 @@ export class DarkmodeService {
 
     if (darkModeElement) {
       if (this.darkOn()) {
-        darkModeElement.classList.add('dark');
-        darkModeElement.classList.remove('light');
+        this.setDark(darkModeElement);
       } else {
-        darkModeElement.classList.add('light');
-        darkModeElement.classList.remove('dark');
+        this.setLight(darkModeElement);
       }
     }
   }
 
-  getMode(): boolean {
-    return this.darkOn();
+  setDark(darkModeElement: HTMLElement): void {
+    localStorage.setItem(this.key, 'dark');
+    darkModeElement.classList.add('dark');
+    darkModeElement.classList.remove('light');
+  }
+
+  setLight(darkModeElement: HTMLElement): void {
+    localStorage.setItem(this.key, 'light');
+    darkModeElement.classList.add('light');
+    darkModeElement.classList.remove('dark');
+  }
+
+  isDark(): boolean {
+    const lStorage: string | null = localStorage.getItem(this.key);
+    if (lStorage == null) {
+      return false;
+    }
+    if (lStorage == 'dark') {
+      return true;
+    } else {
+      return false;
+    }
   }
 }

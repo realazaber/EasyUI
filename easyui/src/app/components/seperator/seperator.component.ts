77f72c2b1695a0826e45732b085e68700371bce4
AZ <47402666+realazaber@users.noreply.github.com>
2024-06-21 21:12:02 +0200

    Dummy component: Seperator

---
+++ easyui/src/app/components/seperator/seperator.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seperator',
+  standalone: true,
+  imports: [],
+  templateUrl: './seperator.component.html',
+  styles: ``
+})
+export class SeperatorComponent {
+
+}

9c11e028df38160ca4d781f4205f4a36f51532ec
AZ <47402666+realazaber@users.noreply.github.com>
2024-06-21 21:11:16 +0200

    QOL: Add col and row to layout module

--- easyui/projects/ngeasy-ui/src/lib/modules/layout/layout.module.ts
+++ easyui/projects/ngeasy-ui/src/lib/modules/layout/layout.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EuiSectionComponent } from '../../components/section/section.component';
 import { EuiContainerComponent } from '../../components/container/container.component';
+import { EuiColComponent } from '../../components/col/col.component';
+import { EuiRowComponent } from '../../components/row/row.component';
 
 @NgModule({
   declarations: [],
-  imports: [EuiSectionComponent, EuiContainerComponent, CommonModule],
-  exports: [EuiSectionComponent, EuiContainerComponent, CommonModule],
+  imports: [
+    EuiSectionComponent,
+    EuiContainerComponent,
+    EuiColComponent,
+    EuiRowComponent,
+    CommonModule,
+  ],
+  exports: [
+    EuiSectionComponent,
+    EuiContainerComponent,
+    EuiColComponent,
+    EuiRowComponent,
+    CommonModule,
+  ],
 })
 export class EuiLayoutModule {}

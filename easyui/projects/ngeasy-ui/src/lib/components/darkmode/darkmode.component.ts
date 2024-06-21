1c49887a7772d13656b429fb6aa4afed16519935
AZ <47402666+realazaber@users.noreply.github.com>
2024-06-21 21:10:51 +0200

    Feature: Dark mode component works with local storage

--- easyui/projects/ngeasy-ui/src/lib/components/darkmode/darkmode.component.ts
+++ easyui/projects/ngeasy-ui/src/lib/components/darkmode/darkmode.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { DarkmodeService } from '../../services/darkmode.service';
 
 @Component({
@@ -12,29 +12,40 @@ import { DarkmodeService } from '../../services/darkmode.service';
 export class EuiDarkModeComponent implements OnInit {
   classes: string = '';
   label: string = 'Button';
+  toggleDiv: HTMLElement | null = window.document.getElementById('darkWrapper');
+  @Input() lightLabel: string = 'Light ☀️';
+  @Input() darkLabel: string = 'Dark 🌙';
 
   constructor(private darkMode: DarkmodeService) {}
 
   ngOnInit(): void {
+    console.log('mode: ' + this.darkMode.isDark());
     this.getClasses();
+    if (!this.toggleDiv) {
+      return;
+    }
+    if (this.darkMode.isDark()) {
+      this.darkMode.setDark(this.toggleDiv);
+    } else {
+      this.darkMode.setLight(this.toggleDiv);
+    }
   }
 
   toggleMode(): void {
-    let toggleDiv: HTMLElement | null =
-      window.document.getElementById('darkWrapper');
-    if (toggleDiv) {
-      this.darkMode.toggleDarkMode(toggleDiv);
+    if (this.toggleDiv) {
+      this.darkMode.toggleDarkMode(this.toggleDiv);
     }
     this.getClasses();
   }
 
   getClasses(): string {
-    if (this.darkMode.getMode() == true) {
+    if (this.darkMode.isDark() == true) {
       //dark
       this.classes =
         'w-24 hover:cursor-pointer p-1 rounded-full bg-white text-black border-2 border-slate-300 ';
     } else {
       //light
+
       this.classes =
         'w-24 hover:cursor-pointer p-1 rounded-full bg-black text-white ';
     }
@@ -42,10 +53,10 @@ export class EuiDarkModeComponent implements OnInit {
   }
 
   getLabel(): string {
-    if (this.darkMode.getMode() == true) {
-      this.label = 'Light ☀️';
+    if (this.darkMode.isDark() == true) {
+      this.label = this.lightLabel;
     } else {
-      this.label = 'Dark 🌙';
+      this.label = this.darkLabel;
     }
     return this.label;
   }
